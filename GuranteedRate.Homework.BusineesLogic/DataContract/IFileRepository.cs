3b1c64e04c20846083494bd3baa564fbd955155c
VaidyaKev <kevinv1990@gmail.com>
2022-02-11 16:56:02 -0500

    added file repository

---
+++ GuranteedRate.Homework.BusineesLogic/DataContract/IFileRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace GuranteedRate.Homework.BusineesLogic.DataContract
+{
+    public interface IFileRepository
+    {
+        int GetFileCount(string folderPath);
+        string GetFileContent(string folderPath, string fileName);
+        void WriteFileContent(string folderPath, string fileName);
+    }
+}
---
+++ GuranteedRate.Homework.Repository/FileRepository.cs
@@ -0,0 +1,23 @@
+﻿using GuranteedRate.Homework.BusineesLogic.DataContract;
+using System;
+
+namespace GuranteedRate.Homework.Repository
+{
+    public class FileRepository : IFileRepository
+    {
+        public string GetFileContent(string folderPath, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetFileCount(string folderPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteFileContent(string folderPath, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
