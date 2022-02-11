7c581dbdf435002e4123447d26b82a8a60a4c6e9
VaidyaKev <kevinv1990@gmail.com>
2022-02-11 17:47:04 -0500

    added swagger

--- GuranteedRate.Homework.API/Startup.cs
+++ GuranteedRate.Homework.API/Startup.cs
@@ -21,6 +21,7 @@ namespace GuranteedRate.Homework.API
         {
             services.AddControllers();
             DI.ConfigureServices(services);
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +30,12 @@ namespace GuranteedRate.Homework.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
             }
 
             app.UseHttpsRedirection();
