bfbcedfe12d359f03338368cf865d4f779e59518
VaidyaKev <kevinv1990@gmail.com>
2022-02-11 17:42:00 -0500

    test controller work

---
+++ GuranteedRate.Homework.API/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using GuranteedRate.Homework.BusineesLogic.DataContract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuranteedRate.Homework.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        private readonly IRecordBusinessLogic _recordBusinessLogic;
+
+        public TestController(IRecordBusinessLogic recordBusinessLogic)
+        {
+            _recordBusinessLogic = recordBusinessLogic;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _recordBusinessLogic.GetRecord("");
+        }
+    }
+}
