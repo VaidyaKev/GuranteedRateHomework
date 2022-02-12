49c75f88674c2e7ea772753390269c67d20fca55
VaidyaKev <kevinv1990@gmail.com>
2022-02-11 19:11:50 -0500

    seperated DI proj

---
+++ GuranteedRate.Homeword.DI/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using GuranteedRate.Homework.BusineesLogic.DataContract;
+using GuranteedRate.Homework.BusineesLogic.Implementations;
+using GuranteedRate.Homework.BusineesLogic.Interfaces;
+using GuranteedRate.Homework.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace GuranteedRate.Homeword.DI
+{
+    public static class DependencyInjection
+    {
+        public static void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddScoped<IRecordBusinessLogic, RecordBusinessLogic>()
+                .AddScoped<IFileRepository, FileRepository>();
+        }
+    }
+}
