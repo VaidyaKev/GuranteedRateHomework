3ea65dd1ced8de1660b0160766265c50e23f7998
VaidyaKev <kevinv1990@gmail.com>
2022-02-11 19:41:13 -0500

    able to display in table

--- GuranteedRate.Homework.Command/Executor.cs
+++ GuranteedRate.Homework.Command/Executor.cs
@@ -1,4 +1,7 @@
-﻿using GuranteedRate.Homework.BusineesLogic.Interfaces;
+﻿using ConsoleTables;
+using GuranteedRate.Homework.BusineesLogic.Interfaces;
+using GuranteedRate.Homework.Domain.Extensions;
+using GuranteedRate.Homework.Model;
 using System;
 
 namespace GuranteedRate.Homework.Command
@@ -14,13 +17,25 @@ namespace GuranteedRate.Homework.Command
 
         public void Execute()
         {
-            _recordBusinessLogic.AddRecord($"added this record at {DateTime.Now}");
+            var person = new Person()
+            {
+                FirstName = "Joe",
+                LastName = "Doe",
+                Email = "JoeDoe@Domain.com",
+                DateOfBirth = DateTime.Now,
+                FavoriteColor = "Blue"
+            };
+
+            _recordBusinessLogic.AddRecord(person);
             var records = _recordBusinessLogic.GetRecords();
 
+            var table = new ConsoleTable("FirstName", "LastName", "Email", "DOB", "FavoriteColor");
             foreach(var record in records)
             {
-                Console.WriteLine(record);
+                table.AddRow(record.FirstName, record.LastName, record.Email, record.DateOfBirth.ToString("d"), record.FavoriteColor);
             }
+
+            table.Write();
         }
     }
 }
